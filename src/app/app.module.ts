7a946aad156863cfb12dcb615410f1a885c865df
Kristine <kristineipolakovai@gmail.com>
2022-10-06 19:12:47 +0300

    added get post and add post with ngrx-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { PostsDataService } from './posts/posts-data.service';
+import { HttpClientModule } from '@angular/common/http';
 
+import { environment } from './../environments/environment';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { SinglePostComponent } from './posts/single-post/single-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+import { HomeComponent } from './home/home.component';
+import { EffectsModule } from '@ngrx/effects';
+import { EntityDataModule, EntityDataService } from '@ngrx/data';
+import { entityConfig } from './posts-entity-metadata';
+import { PostsResolver } from './posts/posts.resolver';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    SinglePostComponent,
+    EditPostComponent,
+    AddPostComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+    EntityDataModule.forRoot(entityConfig),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PostsDataService, PostsResolver],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    entityDataService: EntityDataService,
+    PostsDataService: PostsDataService
+  ) {
+    entityDataService.registerService('Post', PostsDataService);
+  }
+}
